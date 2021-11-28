c72fff475c9a23b051384337feabcafe77e6d5c5
Abhinav-Anil <abhinav.anil2206@gmail.com>
2021-11-29 00:13:19 +0530

    feat: add insert list using slice method

---
+++ List/insert_list.py
@@ -0,0 +1,7 @@
+#Program to insert a list in another list using the slice operations
+
+num_list = [1,2,3,4,5,6,7,8,9,10]
+print("Original list :", num_list)
+
+num_list[2] = [3,5,7]
+print("After inserting another list, the updated list is :", num_list)
