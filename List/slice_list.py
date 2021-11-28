b085f441450695fd5390d22d977635a9309e6051
Abhinav-Anil <abhinav.anil2206@gmail.com>
2021-11-28 23:54:11 +0530

    feat: add programming for LIST

---
+++ List/slice_list.py
@@ -0,0 +1,8 @@
+#Program to demonstrate the slice operations used to access the elements of the list 
+
+num_list = [1,2,3,4,5,6,7,8,9,10]
+print("num list is :", num_list)
+print("First element in the list :", num_list[0])
+print("num_list[2:5] =", num_list[2:5])
+print("num_list[::2] =", num_list[::2])
+print("num_list[1::3] =", num_list[1::3])
